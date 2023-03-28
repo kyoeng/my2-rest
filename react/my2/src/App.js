575dc47f76e8abc83c2437499d91d033327d64fa
kyoeng <rudwlsdn@naver.com>
2023-03-28 23:42:01 +0900

    react start

---
+++ react/my2/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <Router>
+
+    </Router>
+  );
+}
+
+export default App;
